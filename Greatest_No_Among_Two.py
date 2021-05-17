f7c3bba8b3702301902401fae47e64430d86b129
Shubham_Rawat <shubhamrawat140798@gmail.com>
2021-05-17 21:15:15 +0530

    Initial Commit

---
+++ Greatest_No_Among_Two.py
@@ -0,0 +1,9 @@
+a = int(input(" Enter first No. : "))
+b = int(input(" Enter second No. : "))
+
+if(a>b):
+	print(" a: ",a," is greater than b: ",b)
+elif(a==b):
+	print(" a: ",a," is equal b: ",b)
+else:
+	print(" b: ",b," is greater than a: ",a)
