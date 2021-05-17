c12162217123114a515cb675a508d7601fa5ab32
Shubham_Rawat <shubhamrawat140798@gmail.com>
2021-05-17 22:07:45 +0530

    Initial Commit

---
+++ If_else/Medium/Evaluate_Student_Performance.py
@@ -0,0 +1,11 @@
+Marks = int(input("Enter student Marks: "))
+if(Marks>=90):
+	print("Excellent performance")
+elif(Marks>=80):
+	print("Very Good performance")
+elif(Marks>=70):
+	print("Good performance")
+elif(Marks>=60):
+	print("Average performance")
+else:
+	print("Poor performance")
