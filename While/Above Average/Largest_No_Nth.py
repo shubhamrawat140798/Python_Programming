2f5702e657e04a686be9ef7fd8dbdb176728877c
Shubham_Rawat <shubhamrawat140798@gmail.com>
2021-05-18 21:00:33 +0530

    Initial Commit

---
+++ While/Above Average/Largest_No_Nth.py
@@ -0,0 +1,10 @@
+lst = []
+n = int(input("Enter how many number you want to input : "))
+i=0
+
+while(i<n):
+    value = int(input(" Enter  No. : "))
+    lst.append(value)
+    i+=1
+print("Maximum No. : ", max(lst))
+    
